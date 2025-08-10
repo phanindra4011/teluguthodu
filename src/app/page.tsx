7d16b07bff1e8878dd8f23abfa218460141c3dc2
Sudhatri Nandimalla <sudhatrisarma@gmail.com>
2025-08-10 07:02:33 +0000

    it is showing is but i  want main interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 export default function Home() {
   return (
     <SidebarProvider>
-      <div className="flex flex-col h-screen bg-background text-foreground font-body">
-        <ChatView />
-      </div>
+      <ChatView />
     </SidebarProvider>
   );
 }
